0dba00f6cabdf50ca701477c8d603118615b8def
Elliot Fernandez <elliot@hispantic.com>
2024-02-23 21:07:42 +0000

    Canvis menors

--- public/header_html.php
+++ public/header_html.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Finguer - Parking Aeropuerto Barcelona El Prat T1 y T2</title>
-    <link rel="icon" href="/public/img/favicon.png" type="image/png">
+    <link rel="icon" href="/img/favicon.png" type="image/png">
 
     <!-- Agrega los scripts de Stripe y jQuery -->
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
