f5d9621a2973c392bdca35d17b5a5e0174b88dd6
Elliot Fernandez <elliot@hispantic.com>
2026-06-10 13:15:29 +0200

    Correcció errors frontend

--- public/includes/header.php
+++ public/includes/header.php
@@ -1,13 +1,13 @@
 <?php
 // Obtener el idioma actual desde la cookie
-$currentLanguage = $_COOKIE['language'] ?? 'es';  // Si no está establecido, por defecto 'es'
+$currentLanguage = $_COOKIE['language'] ?? 'es'; // Si no está establecido, por defecto 'es'
 
 // Idiomas disponibles
 $languages = [
-  'es' => 'Español',
-  'fr' => 'Français',
-  'en' => 'English',
-  'ca' => 'Català',
+    'es' => 'Español',
+    'fr' => 'Français',
+    'en' => 'English',
+    'ca' => 'Català',
 ];
 
 // Obtener la URL actual sin el idioma (comenzando desde el primer segmento después de la raíz)
@@ -16,12 +16,12 @@ $currentUri = $_SERVER['REQUEST_URI'];
 // Eliminar el idioma actual de la URL (por ejemplo, de '/es', '/fr', '/en', '/ca')
 $baseUri = preg_replace('#^/(fr|en|ca)/#', '/', $currentUri);
 
-$base_url = ($currentLanguage === 'es') ? '/' : "/$currentLanguage/";
+$base_url = $currentLanguage === 'es' ? '/' : "/$currentLanguage/";
 
 // Si el idioma actual es español, no agregar el prefijo '/es'
 if ($currentLanguage === 'es') {
-  // Si el idioma es español, la URL debe ser simplemente '/reserva' o cualquier página sin el prefijo '/es'
-  $baseUri = preg_replace('#^/es/#', '/', $currentUri);
+    // Si el idioma es español, la URL debe ser simplemente '/reserva' o cualquier página sin el prefijo '/es'
+    $baseUri = preg_replace('#^/es/#', '/', $currentUri);
 }
 
 // Obtener traducciones generales
@@ -38,6 +38,7 @@ $generalTranslations = $translations['header'] ?? [];
   <meta name="description" content="Finguer es un parking para coches con servicio de traslado y recogida al aeropuerto de Barcelona. Pero nosotros nos consideramos más como un hotel para mascotas.">
   <meta name="keywords" content="Parking, Aeropuerto, El Prat, finguer, traslado">
   <link rel="icon" href="<?php APP_ROOT; ?>/img/favicon.png" type="image/png">
+  <script src="https://cdnjs.cloudflare.com/polyfill/v3/polyfill.min.js?version=4.8.0&features=default,Promise,fetch,Array.from,Object.assign"></script>
   <!-- Bootstrap 5 CSS -->
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
   <!-- CSS personalizado -->
@@ -50,7 +51,8 @@ $generalTranslations = $translations['header'] ?? [];
   <div class="container d-flex flex-column" style="flex: 1;">
     <nav class="navbar navbar-expand-lg bg-body-tertiary">
       <div class="container-fluid">
-        <a href="<?php echo APP_WEB . $base_url ?>"><img alt="Finguer" class="img-responsive" src="/img/logo-header.svg"></a>
+        <a href="<?php echo APP_WEB .
+            $base_url; ?>"><img alt="Finguer" class="img-responsive" src="/img/logo-header.svg"></a>
 
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
@@ -59,23 +61,38 @@ $generalTranslations = $translations['header'] ?? [];
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
           <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
             <li class="nav-item">
-              <a class="nav-link" aria-current="page" href="<?php echo APP_WEB . $base_url ?>reserva"><?php echo ($generalTranslations['home'] ?? 'Inicio') ?></a>
+              <a class="nav-link" aria-current="page" href="<?php echo APP_WEB .
+                  $base_url; ?>reserva"><?php echo $generalTranslations[
+    'home'
+] ?? 'Inicio'; ?></a>
             </li>
 
             <li class="nav-item">
-              <a class="nav-link" href="<?php echo APP_WEB . $base_url ?>#servicios"><?php echo ($generalTranslations['servicios'] ?? 'Servicios') ?></a>
+              <a class="nav-link" href="<?php echo APP_WEB .
+                  $base_url; ?>#servicios"><?php echo $generalTranslations[
+    'servicios'
+] ?? 'Servicios'; ?></a>
             </li>
 
             <li class="nav-item">
-              <a class="nav-link" href="<?php echo APP_WEB . $base_url ?>#formulario"><?php echo ($generalTranslations['donde'] ?? 'Dónde estamos') ?></a>
+              <a class="nav-link" href="<?php echo APP_WEB .
+                  $base_url; ?>#formulario"><?php echo $generalTranslations[
+    'donde'
+] ?? 'Dónde estamos'; ?></a>
             </li>
 
             <li class="nav-item">
-              <a class="nav-link" href="<?php echo APP_WEB . $base_url ?>#formulario"><?php echo ($generalTranslations['contacto'] ?? 'Contacto') ?></a>
+              <a class="nav-link" href="<?php echo APP_WEB .
+                  $base_url; ?>#formulario"><?php echo $generalTranslations[
+    'contacto'
+] ?? 'Contacto'; ?></a>
             </li>
 
             <li class="nav-item">
-              <a class="nav-link" href="<?php echo APP_WEB . $base_url ?>area-cliente/"><?php echo ($generalTranslations['micuenta'] ?? 'Mi cuenta') ?></a>
+              <a class="nav-link" href="<?php echo APP_WEB .
+                  $base_url; ?>area-cliente/"><?php echo $generalTranslations[
+    'micuenta'
+] ?? 'Mi cuenta'; ?></a>
             </li>
           </ul>
 
@@ -83,12 +100,18 @@ $generalTranslations = $translations['header'] ?? [];
         <!-- Menú de idiomas con Bootstrap -->
         <div class="dropdown">
           <button class="btn btn-secondary dropdown-toggle" type="button" id="languageDropdown" data-bs-toggle="dropdown" aria-expanded="false">
-            <?php echo $languages[$currentLanguage]; ?> <!-- Mostrar el idioma actual -->
+            <?php echo $languages[
+                $currentLanguage
+            ]; ?> <!-- Mostrar el idioma actual -->
           </button>
           <ul class="dropdown-menu" aria-labelledby="languageDropdown">
             <?php foreach ($languages as $langCode => $langName): ?>
               <li>
-                <a class="dropdown-item" href="<?php echo $langCode === 'es' ? $baseUri : '/' . $langCode . $baseUri ?>"> <!-- Redirigir al mismo URI con el nuevo idioma -->
+                <a class="dropdown-item" href="<?php echo $langCode === 'es'
+                    ? $baseUri
+                    : '/' .
+                        $langCode .
+                        $baseUri; ?>"> <!-- Redirigir al mismo URI con el nuevo idioma -->
                   <?php echo $langName; ?>
                 </a>
               </li>
