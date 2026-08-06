d1ae0eb7449971204684728e66ebf0a236a291c9
Elliot Fernandez <elliot@hispantic.com>
2026-08-06 19:52:17 +0200

    Modificat namespace de la clase ApiResponse i JsonResponse

--- src/backend/Infrastructure/EntryPoint/Http/Response/ApiResponse/ApiResponse.php
+++ src/backend/Infrastructure/EntryPoint/Http/Response/ApiResponse/ApiResponse.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace App\Infrastructure\EntryPoint\Http\Response;
+namespace App\Infrastructure\EntryPoint\Http\Response\ApiResponse;
 
 final class ApiResponse
 {
